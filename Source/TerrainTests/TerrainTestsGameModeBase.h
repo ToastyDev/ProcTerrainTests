257502184200c3074ef27319c5dabf882eac5101
Toasty <curtisgauntdev@gmail.com>
2020-08-09 21:54:27 -0400

    Initial project build

---
+++ Source/TerrainTests/TerrainTestsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TerrainTestsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TERRAINTESTS_API ATerrainTestsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
